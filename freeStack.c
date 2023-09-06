979befbf6735c994faf6fd7d068c0acc4870be77
electrikbox <electrikbox@icloud.com>
2023-09-06 11:14:13 +0200

    fix for betty

--- freeStack.c
+++ freeStack.c
@@ -3,12 +3,14 @@
 /* Function to free the stack at the end of the program */
 void free_stack(stack_t **stack)
 {
-    stack_t *current = *stack;
-    while (current != NULL)
-    {
-        stack_t *temp = current;
-        current = current->next;
-        free(temp);
-    }
-    *stack = NULL;
+	stack_t *current = *stack;
+	stack_t *temp;
+
+	while (current != NULL)
+	{
+		temp = current;
+		current = current->next;
+		free(temp);
+	}
+	*stack = NULL;
 }
