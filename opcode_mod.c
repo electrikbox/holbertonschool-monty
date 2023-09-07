63c0ef5867dc884f39d0b85fe223833709767c9e
BIDcolonel <ptitofdu89@live.fr>
2023-09-07 14:42:02 +0200

    zero management added

--- opcode_mod.c
+++ opcode_mod.c
@@ -13,6 +13,12 @@ void opcode_mod(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
 	(*stack)->next->n %= (*stack)->n;
 	opcode_pop(stack, line_number);
 }
