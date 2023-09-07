ab6877ec02f67affe40409a35965cf50da15b419
BIDcolonel <ptitofdu89@live.fr>
2023-09-08 00:40:45 +0200

    correct betty style ...

--- opcode_pstr.c
+++ opcode_pstr.c
@@ -7,15 +7,15 @@
  */
 void opcode_pstr(stack_t **stack, unsigned int line_number)
 {
-    stack_t *current = *stack;
+	stack_t *current = *stack;
 
-    (void)line_number; // Unused parameter
+	(void)line_number;
 
-    while (current != NULL && current->n != 0 && (current->n >= 0 && current->n <= 127))
-    {
-        printf("%c", current->n);
-        current = current->next;
-    }
+	while (current != NULL && current->n != 0 && (current->n >= 0 && current->n <= 127))
+	{
+		printf("%c", current->n);
+		current = current->next;
+	}
 
-    printf("\n");
+	printf("\n");
 }
--- opcode_rotl.c
+++ opcode_rotl.c
@@ -7,18 +7,18 @@
  */
 void opcode_rotl(stack_t **stack, unsigned int line_number)
 {
-    stack_t *temp = *stack;
-    (void)line_number; // Unused parameter
+	stack_t *temp = *stack;
+	(void)line_number;
 
-    if (*stack == NULL || (*stack)->next == NULL)
-        return;
+	if (*stack == NULL || (*stack)->next == NULL)
+		return;
 
-    while (temp->next != NULL)
-        temp = temp->next;
+	while (temp->next != NULL)
+		temp = temp->next;
 
-    temp->next = *stack;
-    (*stack)->prev = temp;
-    *stack = (*stack)->next;
-    (*stack)->prev = NULL;
-    temp->next->next = NULL;
+	temp->next = *stack;
+	(*stack)->prev = temp;
+	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
+	temp->next->next = NULL;
 }
